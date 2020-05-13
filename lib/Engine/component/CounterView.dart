957b2c7278ba7eac45b9e4de793fb3be086759ce
micmine <micmine4@gmail.com>
2020-05-13 19:52:43 +0200

    Migrating to AndroidX

--- lib/Engine/component/CounterView.dart
+++ lib/Engine/component/CounterView.dart
@@ -2,89 +2,96 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class CounterView extends StatefulWidget {
-  CounterView(int id, String name, Color color) {
-    this.id = id;
-    this.name = name;
-    //this.value = value;
-    this.color = color;
-  }
+	CounterView(int id, String name, Color color) {
+		this.id = id;
+		this.name = name;
+		//this.value = value;
+		this.color = color;
+	}
 
-  int id;
-  String name;
+	int id;
+	String name;
 
-  //int value;
-  Color color;
+	//int value;
+	Color color;
 
-  @override
-  _CounterViewState createState() =>
-      _CounterViewState(this.id, this.name, this.color);
+	@override
+	_CounterViewState createState() =>
+	_CounterViewState(this.id, this.name, this.color);
 }
 
 class _CounterViewState extends State<CounterView> {
-  _CounterViewState(int id, String name, Color color) {
-    this.id = id;
-    this.name = name;
-    //this.value = value;
-    this.value = 0;
-    this.color = color;
-  }
+	_CounterViewState(int id, String name, Color color) {
+		this.id = id;
+		this.name = name;
+		//this.value = value;
+		this.value = 0;
+		this.color = color;
+	}
 
-  int id;
-  String name;
-  int value;
-  Color color;
+	int id;
+	String name;
+	int value;
+	Color color;
 
-  @override
-  Widget build(BuildContext context) {
-    initValue();
-    return Container(
-        alignment: AlignmentDirectional.center,
-        color: color,
-        child: Column(
-          children: <Widget>[
-            Text(name.toString()),
-            Text(
-              value.toString(),
-              style: TextStyle(fontSize: 100.0, color: Colors.white),
-            ),
-            RaisedButton(
-              child: Text("increase"),
-              onPressed: _increase,
-            ),
-            RaisedButton(
-              child: Text("decrease"),
-              onPressed: _decrease,
-            )
-          ],
-        ));
-  }
+	@override
+	Widget build(BuildContext context) {
+		initValue();
+		return Container(
+				alignment: AlignmentDirectional.center,
+				color: color,
+				child: Column(
+						children: <Widget>[
+							Text(name.toString()),
+							Text(
+									value.toString(),
+									style: TextStyle(fontSize: 100.0, color: Colors.white),
+							),
+							RaisedButton(
+									child: Text("increase"),
+									onPressed: _increase,
+							),
+							RaisedButton(
+									child: Text("decrease"),
+									onPressed: _decrease,
+							)
+						],
+				));
+	}
 
-  _increase() {
-    setState(() {
-      value++;
-    });
-    _save();
-  }
+	_increase() {
+		setState(() {
+			if (value == null) {
+				value = 0;
+			}
+			value++;
+		});
+		_save();
+	}
 
-  _decrease() {
-    setState(() {
-      value--;
-    });
-    _save();
-  }
+	_decrease() {
+		setState(() {
+			if (value == null) {
+				value = 0;
+			}
 
-  _save() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    int counter = prefs.getInt('counter-' + id.toString());
-    print('Pressed $counter times.');
-    await prefs.setInt('counter-' + id.toString(), value);
-    print('|-| Pressed $counter times.');
-  }
+			value--;
+		});
+		_save();
+	}
 
-  initValue() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    setState(() {
-      this.value = prefs.getInt('counter-' + id.toString());
-    });
-  }
+	_save() async {
+		SharedPreferences prefs = await SharedPreferences.getInstance();
+		int counter = prefs.getInt('counter-' + id.toString());
+		print('Pressed $counter times.');
+		await prefs.setInt('counter-' + id.toString(), value);
+		print('|-| Pressed $counter times.');
+	}
+
+	initValue() async {
+		SharedPreferences prefs = await SharedPreferences.getInstance();
+		setState(() {
+			this.value = prefs.getInt('counter-' + id.toString());
+		});
+	}
 }
